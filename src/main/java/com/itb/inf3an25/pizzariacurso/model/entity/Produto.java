2381ff96d30e45263d00cb3fc60121751a5d34b3
Giovanna Lourenço Tesoro <rm90620@estudante.fieb.edu.br>
2025-02-17 20:12:48 -0300

    Camada model entity em construção

---
+++ src/main/java/com/itb/inf3an25/pizzariacurso/model/entity/Produto.java
@@ -0,0 +1,43 @@
+package com.itb.inf3an25.pizzariacurso.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "produtos")
+
+
+public class Produto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String nome;
+    @Column(nullable = true, length = 255)
+    private String descricao;
+   @Column(nullable = true, length = 45)
+    private String tipo;
+    @Column(nullable = true)
+    private int quantidadeEstoque;
+    @Column(nullable = true, columnDefinition = "DECIMAL(5,2)")
+    private double precoVenda;
+    @Column(nullable =true, columnDefinition = "DECIMAL(5,2)" )
+    private double precoCompra;
+   private boolean codStatus;
+
+    // Atributos de apoio 
+    
+    @Transient   //Informa que o atributo Não é coluna de bando de dados
+    private String mensagemErro = "";
+    @Transient
+    private boolean isValid = true;
+
+    
+
+}
